361e27ed46ee47f8a80578de75777dc431243243
dakewamama <davidasiadiachi@gmail.com>
2025-10-15 03:15:53 +0000

    Remove large files exceeding 100MB

--- anchor-vault-starter-q4-25/programs/anchor-vault-q4-25/src/lib.rs
+++ anchor-vault-starter-q4-25/programs/anchor-vault-q4-25/src/lib.rs
@@ -1,9 +1,8 @@
 use anchor_lang::{
-    prelude::*,
-    system_program::{transfer, Transfer},
+    accounts::account, prelude::*, system_program::{transfer, Transfer}
 };
 
-declare_id!("2u5cG7PEVL5KdTRMWSjdwqtBVv1anE5Hvv4FGSPZVRUN");
+declare_id!("GKoLPJ26rCsZemX27KXJJB9foyEZDwKz4jQUVuJYMvBH");
 
 #[program]
 pub mod anchor_vault_q4_25 {
@@ -17,13 +16,13 @@ pub mod anchor_vault_q4_25 {
         ctx.accounts.deposit(amount)
     }
 
-    // pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
-    //     ctx.accounts.withdraw(amount)
-    // }
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
+        ctx.accounts.withdraw(amount)
+    }
 
-    // pub fn close(ctx: Context<Close>) -> Result<()> {
-    //     ctx.accounts.close()
-    // }
+    pub fn close(ctx: Context<Close>) -> Result<()> {
+        ctx.accounts.close()
+    }
 }
 
 #[derive(Accounts)]
@@ -105,30 +104,83 @@ impl<'info> Deposit<'info> {
     }
 }
 
-// #[derive(Accounts)]
-// pub struct Withdraw<'info> {
-// TODO: Implement Withdraw accounts
-// }
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+//TODO: Implement Withdraw accounts
+   #[account(mut)]
+   pub vault: SystemAccount<'info>,
+   #[account(
+    seeds = [b"state", user.key().as_ref()],
+    bump = vault_state.state_bump,
 
-// impl<'info> Withdraw<'info> {
-//     pub fn withdraw(&mut self, _amount: u64) -> Result<()> {
-//         TODO: Implement withdraw
+    pub vault_state: Account<'info, VaultState>,
+    pub system_program: Program<'info, System>,
+   )]
+}
 
-//         Ok(())
-//     }
-// }
+impl<'info> Withdraw<'info> {
+    pub fn withdraw(&mut self, _amount: u64) -> Result<()> {
+        //TODO: Implement withdraw
+        let vault_state_key = self.vault_state.key().clone();
+        let signer_seeds: &[&[&[u8]]] = &[&[b"vault", vault_state_key.as_ref(), &[self.vault_state.vault_bump]]];
 
-// #[derive(Accounts)]
-// pub struct Close<'info> {
-//      TODO: Implement Close accounts
-// }
+        let cpi_program = self.system_program.to_account_info();
+        let cpi_accounts = Transfer {
+             from: self.vault.to_account_info(), 
+             to: self.user.to_account_info(),
+        };
 
-// impl<'info> Close<'info> {
-//     pub fn close(&mut self) -> Result<()> {
-//          TODO: Implement close
-//         Ok(())
-//     }
-// }
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+        transfer(cpi_ctx, amount)?;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Close<'info> {
+     //TODO: Implement Close accounts
+    
+        #[account(mut)]
+        pub user: Signer<'info>,
+        #[account(
+            mut,
+            seeds = [b"vault", vault_state.key().as_ref()],
+            bump = vault_state.vault_bump,
+    )]
+    pub vault: SystemAccount<'info>,
+    #[account(
+        mut,
+        seeds = [b"state", user.key().as_ref()],
+        bump = vault_state.state_bump,
+        close = user,
+        )]
+    pub vault_state: Account<'info, VaultState>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Close<'info> {
+    pub fn close(&mut self) -> Result<()> {
+         //TODO: Implement close
+        let cpi_program = self.system_program.to_account_info();
+
+        let cpi_accounts = Transfer {
+            from: self.vault.to_account_info(),
+            to: self.user.to_account_info(),
+        };
+
+        let signer_seeds: &[&[&[u8]]] = &[&[
+            b"vault",
+            self.vault_state.to_account_info().key.as_ref(),
+            &[self.vault_state.vault_bump],
+        ]];
+        
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+
+        transfer(cpi_ctx, self.vault.lamports())?;
+
+        Ok(())
+    }
+}
 
 #[derive(InitSpace)]
 #[account]
