cc83c2100ad5ab7de001f8953db987e7935b71c4
dakewamama <davidasiadiachi@gmail.com>
2025-11-03 01:25:38 +0100

    Removed nested git repos and cleaned structure

---
+++ anchor-nft-staking-starter-q4-25/programs/anchor-nft-staking-q4-25/src/instructions/unstake.rs
@@ -0,0 +1,105 @@
+use anchor_lang::prelude::*;
+use mpl_core::{
+    instructions::{RemovePluginV1CpiBuilder, UpdatePluginV1CpiBuilder},
+    types::{FreezeDelegate, Plugin, PluginType},
+    ID as CORE_PROGRAM_ID,
+};
+
+use crate::{
+    errors::StakeError,
+    state::{StakeAccount, StakeConfig, UserAccount},
+};
+
+#[derive(Accounts)]
+pub struct Unstake<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        mut,
+        constraint = asset.owner == &CORE_PROGRAM_ID @ StakeError::InvalidAsset,
+    )]
+    /// CHECK: Verified by mpl-core
+    pub asset: UncheckedAccount<'info>,
+
+    #[account(
+        mut,
+        constraint = collection.owner == &CORE_PROGRAM_ID @ StakeError::InvalidCollection,
+    )]
+    /// CHECK: Verified by mpl-core
+    pub collection: UncheckedAccount<'info>,
+
+    #[account(
+        mut,
+        close = user,
+        seeds = [b"stake", config.key().as_ref(), asset.key().as_ref()],
+        bump = stake_account.bump,
+        constraint = stake_account.owner == user.key() @ StakeError::NotOwner,
+    )]
+    pub stake_account: Account<'info, StakeAccount>,
+
+    #[account(
+        seeds = [b"config"],
+        bump = config.bump,
+    )]
+    pub config: Account<'info, StakeConfig>,
+
+    #[account(
+        mut,
+        seeds = [b"user", user.key().as_ref()],
+        bump = user_account.bump,
+    )]
+    pub user_account: Account<'info, UserAccount>,
+
+    #[account(address = CORE_PROGRAM_ID)]
+    /// CHECK: Verified by address constraint
+    pub core_program: UncheckedAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Unstake<'info> {
+    pub fn unstake(&mut self) -> Result<()> {
+        let time_elapsed =
+            ((Clock::get()?.unix_timestamp - self.stake_account.staked_at) / 86400) as u32;
+
+        require!(
+            time_elapsed >= self.config.freeze_period,
+            StakeError::FreezePeriodNotPassed
+        );
+
+        let points_earned = time_elapsed * self.config.points_per_stake as u32;
+        self.user_account.points += points_earned;
+
+        let signer_seeds: &[&[&[u8]]] = &[&[
+            b"stake",
+            &self.config.key().to_bytes(),
+            &self.asset.key().to_bytes(),
+            &[self.stake_account.bump],
+        ]];
+
+        // First thaw the asset (set frozen: false) using stake_account PDA authority
+        UpdatePluginV1CpiBuilder::new(&self.core_program.to_account_info())
+            .asset(&self.asset.to_account_info())
+            .collection(Some(&self.collection.to_account_info()))
+            .payer(&self.user.to_account_info())
+            .authority(Some(&self.stake_account.to_account_info()))
+            .system_program(&self.system_program.to_account_info())
+            .plugin(Plugin::FreezeDelegate(FreezeDelegate { frozen: false }))
+            .invoke_signed(signer_seeds)?;
+
+        // Then remove the FreezeDelegate plugin using user as authority
+        RemovePluginV1CpiBuilder::new(&self.core_program.to_account_info())
+            .asset(&self.asset.to_account_info())
+            .collection(Some(&self.collection.to_account_info()))
+            .payer(&self.user.to_account_info())
+            .authority(None)
+            .system_program(&self.system_program.to_account_info())
+            .plugin_type(PluginType::FreezeDelegate)
+            .invoke()?;
+
+        self.user_account.amount_staked -= 1;
+
+        Ok(())
+    }
+}
